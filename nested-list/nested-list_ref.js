4c652dca242739a2ce8b134c9a81334ac6289a82
Alex Sova <foxeyes.ml@gmail.com>
2022-07-05 15:53:06 +0300

    feat: nested lists example

---
+++ nested-list/nested-list_ref.js
@@ -0,0 +1,48 @@
+import { BaseComponent } from '../submodules/symbiote/core/BaseComponent.js';
+
+function nestedDataGen(idx) {
+  let data = [];
+  for (let i = 0; i < 3; i++) {
+    data.push({
+      nestedName: 'Nested: ' + (i + 1),
+      nestedText: Date.now(),
+      nestedData: [
+        {
+          nestedName: '111',
+          nestedText: Date.now(),
+        },
+        {
+          nestedName: '222',
+          nestedText: Date.now(),
+        },
+      ],
+    });
+  }
+  return data;
+}
+
+//// Dynamic list wrapper component:
+class NestApp extends BaseComponent {
+  
+  processInnerHtml = true;
+
+  init$ = {
+    data: [],
+    buttonActionName: 'Generate',
+    generateData: () => {
+      this.set$({
+        buttonActionName: 'Update',
+      });
+      let data = [];
+      for (let i = 0; i < 3; i++) {
+        data.push({
+          name: i + 1,
+          nestedData: nestedDataGen(i),
+        });
+      }
+      this.$.data = data;
+    },
+  }
+}
+
+NestApp.reg('nest-app');
